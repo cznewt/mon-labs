7ecbf538c0963d894b45eeac09e96fbf9437b889
Ales Komarek <ales.komarek@newt.cz>
2019-04-17 00:29:49 +0200

    Added alertmanager and kube/node mixins

--- Makefile
+++
@@ -1,7 +0,0 @@
-
-init_mixins:
-	git clone https://github.com/kubernetes-monitoring/kubernetes-mixin
-	cd ./kubernetes-mixin/
-	make dashboards_out
-	cp dashboards_out/* ../grafana-prometheus/config/grafana_dashboards
-
---
+++ init_mixins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/kubernetes-monitoring/kubernetes-mixin
+cd kubernetes-mixin
+jb install
+make dashboards_out
+cp dashboards_out/* ../grafana-prometheus/config/grafana_dashboards
+make prometheus_alerts.yaml
+cp prometheus_alerts.yaml ../grafana-prometheus/config/prometheus_rules/kube_alerts.yml
+make prometheus_rules.yaml
+cp prometheus_rules.yaml ../grafana-prometheus/config/prometheus_rules/kube_rules.yml
