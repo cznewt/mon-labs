a847ecefc24a3e177c1f59dd658d0145bd54e1bc
Ales Komarek <ales.komarek@newt.cz>
2019-11-26 10:07:19 +0100

    Fixed jsonnet install

--- init_jsonnet.sh
+++ init_jsonnet.sh
@@ -7,12 +7,12 @@ if test -f "$JSONNET_BIN"; then
 else
     # sjsonnet much faster jsonnet implementation in java
     wget https://github.com/databricks/sjsonnet/releases/download/0.1.6/sjsonnet.jar
-	chmod +x sjsonnet.jar
-	cp sjsonnet.jar /usr/local/bin/jsonnet
+	sudo chmod +x sjsonnet.jar
+	sudo cp sjsonnet.jar /usr/local/bin/jsonnet
     rm sjsonnet.jar
 fi
 
-apt install -y default-jre
+sudo apt install -y default-jre
 
 echo "Requires Java RE, currently installed:"
 java --version
