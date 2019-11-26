bc3befca627c6dfacba5bf0d9662f47803f83b71
Ales Komarek <ales.komarek@newt.cz>
2019-11-26 11:01:09 +0100

    Updated pip install

--- init_jsonnet_utils.sh
+++ init_jsonnet_utils.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-if test -f jsonnet-utils; then
+sudo apt-get install -y \
+    virtualenv \
+    python3-virtualenv \
+    python3-pip
+
+if test -d jsonnet-utils; then
     echo "jsonnet-utils exists, skipping."
 else
     git clone https://github.com/cznewt/jsonnet-utils.git
     cd jsonnet-utils
     virtualenv -p python3 venv
     . venv/bin/activate
-    pip install -r requirements.txt
-    pip install pyyaml
+    pip3 install -r requirements.txt
+    pip3 install pyyaml
+    pip3 install cookiecutter
     python setup.py install
     cd ..
 fi
