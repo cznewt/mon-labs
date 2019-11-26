9c23080aaf8bea8380ef831e4c437d5faa7bbe73
Ales Komarek <ales.komarek@newt.cz>
2019-11-26 10:03:38 +0100

    Fixed compose install

--- init_docker.sh
+++ init_docker.sh
@@ -22,4 +22,4 @@ sudo apt-get update
 sudo apt-get install -y docker-ce
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod + x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
