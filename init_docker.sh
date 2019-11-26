8f3d154356ffee2d1cc43844cb1d7980cd0488d4
Ales Komarek <ales.komarek@newt.cz>
2019-11-26 10:21:53 +0100

    Docker composer updates

--- init_docker.sh
+++ init_docker.sh
@@ -6,7 +6,8 @@ sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common \
+    make
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
