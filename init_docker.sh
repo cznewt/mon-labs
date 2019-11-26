116b4d37073a43e618a25279680df975552a0cfc
Ales Komarek <ales.komarek@newt.cz>
2019-11-26 09:00:42 +0100

    Little updates

--- init_docker.sh
+++ init_docker.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -19,6 +19,7 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod + x /usr/local/bin/docker-compose
